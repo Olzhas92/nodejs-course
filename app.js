cad59a9554d504150c08d9a2592b1c48cb802586
olzhaskhamzin <khamzin@kolesa.kz>
2025-03-30 20:33:43 +0500

    Sending response

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ const http = require('http');
 const PORT = 3005;
 
 const server = http.createServer((req, res) => {
-    console.log('req.url', req.url);
+    console.log(req);
 
-    console.log('req.method', req.method);
+    const url = req.url;
 
-    console.log('req.headers', req.headers);
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form><body>');
+        res.write('</html>')
+
+        return res.end()
+    }
+
+    if (url === '/message') {
+        res.write('<html>')
+        res.write('<body><h2>Message Page</h2></body>')
+        res.write('</html>')
+
+        return res.end();
+    }
+    
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write("<h1>Hello World!</h1>");
+
+    res.end();
 })
 
-server.listen(PORT, () => {
+server.listen(PORT, () => { 
     console.log(`Server is running on PORT ${PORT}`)
 })
