d02cca1ce754c7be026503c94e710de2f866e54f
olzhaskhamzin <khamzin@kolesa.kz>
2025-03-30 19:07:28 +0500

    Understanding requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require('http');
-const PORT = 3000;
+const PORT = 3005;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log('req.url', req.url);
+
+    console.log('req.method', req.method);
+
+    console.log('req.headers', req.headers);
 })
 
 server.listen(PORT, () => {
